c84b23df681a89ce671d9de21396e61f59d2e360
Afonso247 <afonso.h2002@gmail.com>
2023-08-20 22:53:28 -0300

    Botão de refresh + novas palavras

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function Modal({ estaCorreto, turno, solucao }) {
+    const refresh = () => window.location.reload(true);
+
   return (
     <div className='modal'>
         {estaCorreto && (
@@ -8,7 +10,11 @@ export default function Modal({ estaCorreto, turno, solucao }) {
                 <h1>Você venceu!</h1>
                 <p className='solucao'>{solucao}</p>
                 <p>Você usou {turno} tentativa(s) para achar a resposta ^⁻^</p>
-                <small>Recarregue a página para jogar novamente</small>
+                {/* <small>Recarregue a página para jogar novamente</small> */}
+                <br />
+                <button className='botao' onClick={refresh}>
+                Clique aqui para jogar novamente
+                </button>
             </div>
         )}
         {!estaCorreto && (
@@ -16,7 +22,11 @@ export default function Modal({ estaCorreto, turno, solucao }) {
                 <h1>Que pena!</h1>
                 <p className='solucao'>{solucao}</p>
                 <p>Mais sorte na próxima vez :/</p>
-                <small>Recarregue a página para jogar novamente</small>
+                {/* <small>Recarregue a página para jogar novamente</small> */}
+                <br />
+                <button className='botao' onClick={refresh}>
+                Clique aqui para jogar novamente
+                </button>
             </div>
         )}
     </div>
